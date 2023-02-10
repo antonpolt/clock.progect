bf51e5a2db2538bc1127a5406e53b4e9ded5b768
Anton <toxa.fdg@gmail.com>
2023-02-10 15:40:03 +0200

    almost done

--- js/script.js
+++ js/script.js
@@ -46,34 +46,73 @@ $(document).ready(function(){
     })
   });
 
-  function valideForms(form) {
-    $(form).validate({
-      rules:{
-        name: {
-          required: true,
-          minlenght:2
-        },
-        phone: "required",
-        email: {
-          required: true,
-          email: true
-        }
-      },
-      messages: {
-        name: {
-          required: "Напишіть ім'я",
-          minlenght: jQuery.validator.format("Ведіть {0}  символа!")
-        },
-        phone: "Ведіть ваш номер телефону",
-        email: {
-          required: "Напишіть вашу пошту",
-          email: "Не вірна адреса пошти"
-      }
-    }
-    });
-  };
-  valideForms('#consultation-form');
-  valideForms('#consultation form');
-  valideForms('#order form');
+  function validateForms(form){
+          $(form).validate({
+              rules: {
+                  name: {
+                      required: true,
+                      minlength: 2
+                  },
+                  phone: "required",
+                  email: {
+                      required: true,
+                      email: true
+                  }
+              },
+              messages: {
+                  name: {
+                      required: "Пожалуйста, введите свое имя",
+                      minlength: jQuery.validator.format("Введите {0} символа!")
+                    },
+                  phone: "Пожалуйста, введите свой номер телефона",
+                  email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                  }
+              }
+          });
+      };
+      validateForms('#consultation-form');
+      validateForms('#consultation form');
+      validateForms('#order form');
 
+      $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+      $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+    //smoth scroll and pague up
+
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      }
+      else {
+        $('.pageup').fadeOut();
+      }
+    });
+    $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  new WOW().init();
 });
